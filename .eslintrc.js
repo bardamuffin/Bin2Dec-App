9817d44f115f0e7c12f49f53886c669495411747
bardamuffin <bardamuffin@gmail.com>
2019-07-03 01:50:19 +0200

    initial

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,8 +16,12 @@ module.exports = {
         "sourceType": "module"
     },
     "plugins": [
-        "react"
+        "react-native"
     ],
     "rules": {
+        "react-native/no-unused-styles": 2,
+    "react-native/split-platform-components": 2,
+    "react-native/no-inline-styles": 2,
+    "react-native/no-raw-text": 2,
     }
 };
