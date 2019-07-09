249783da239eb581767f207cc41eb2ff89562780
bardamuffin <bardamuffin@gmail.com>
2019-07-09 19:07:59 +0200

    commit

--- src/components/DataInput/DataInput.js
+++ src/components/DataInput/DataInput.js
@@ -2,13 +2,65 @@ import React, { Component } from 'react';
 import { TextInput, View, StyleSheet } from 'react-native';
 import { Button } from 'react-native-elements';
 
+//https://www.culture-informatique.net/conversion-binaire-decimal-hexadecimal-main/
+//
 export default class DataInput extends Component {
-    state = {  }
+    state = { binary: '' }
+    handleBinaryChange = (val) => {
+        
+        let lastChar = val.slice(-1);
+        //max length: 8 digit
+        //accept only 1 or 0 
+        if( val.length < 9 && lastChar == '0' || lastChar == '1') {
+            this.setState({
+                binary: val
+            })
+            console.log("changed: " + val.slice(-1))
+        } 
+            
+    }
+    convertBinToDec = (val) => {
+        //TODO: Convert Binary to Decimal
+        //slice() take two digit, should take only 1
+        console.log("-------------PARSING-------------")
+        let dec = 0;
+        let step = 1;
+        for(let i = val.length; i > 0; i--)  {
+            console.log("STEP: " + i)
+            console.log("val vaut: " + val);
+            console.log("val length: " + val.length);
+            console.log("val slice: " + i+ " val: " + val.slice(i - 1))
+            console.log("val slice parse: " + parseInt(val.slice(i - 1),10))
+            console.log("multiplied: " +step)
+            console.log("DEC: " + dec + " + " + parseInt(val.slice(i - 1),10) * step )
+            dec = dec + parseInt(val.slice(i - 1),10) * step;
+            step *= 2;
+            console.log("END STEP: " + i)
+        }
+        console.log("-------------END-------------")
+        return dec;
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        if(prevState.binary != this.state.binary) {
+            console.log("different state---processing ....");
+            console.log("converted: " + this.convertBinToDec(this.state.binary))
+        }
+    }
+    convertDecToBin = (val) => {
+        //TODO: convert decimal to binary
+    }
+ 
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.textInput}>
-                    <TextInput style={styles.text} value="Enter Binary"></TextInput>
+                    <TextInput 
+                        style={styles.text}
+                        value={this.state.binary}
+                        keyboardType="numeric"
+                        onChangeText={(val) => this.handleBinaryChange(val)}>    
+                    </TextInput>
                 </View>
                 <View style={styles.textInput}>
                     <TextInput style={styles.text} value="Enter Dec"></TextInput>
